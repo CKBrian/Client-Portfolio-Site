52b8b662189897784f575122fa6eb5474a843ba2
Brian <chumobrian@gmail.com>
2024-10-17 20:41:49 +0300

    feat: Add a HomePage component containing the sub-components for the hero, introduction, portfolio, services, testimonials and contacts sections

---
+++ Portfolio/src/components/landingpage/home-page.tsx
@@ -0,0 +1,21 @@
+import HeroSection from './hero';
+import IntroSection from './intro';
+import ServicesSection from './services';
+import PortfolioSection from './projects';
+import ContactSection from './contacts';
+import TestimonialsSection from './testimonials';
+
+const HomePage = () => {
+    return (
+        <>
+        <HeroSection/>
+        <IntroSection/>
+        <ServicesSection/>
+        <PortfolioSection/>
+        <TestimonialsSection/>
+        <ContactSection/>
+        </>
+    );
+}
+
+export default HomePage;
